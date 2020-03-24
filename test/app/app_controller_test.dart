93dd3867a908bc00983fa34ef8b8ec80f3b04566
Isadora Oliveira <oliveiraisadora119@gmail.com>
2020-03-24 14:38:33 -0300

    adding scripts in pubspec

--- test/app/app_controller_test.dart
+++ test/app/app_controller_test.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bank_app/bank_app/app/app_controller.dart';
-import 'package:bank_app/bank_app/app/app_module.dart';
+import 'package:bank_app/app/app_controller.dart';
+import 'package:bank_app/app/app_module.dart';
 
 void main() {
   initModule(AppModule());
